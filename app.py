d8b91912f3a0685d1e64517c3439153ff5633945
Md. Ashraf Uddin <108550504+AshrafUAsif@users.noreply.github.com>
2024-04-06 18:46:45 +0000

    Setup the main 'base.html' file which will be the default home page with the name 'EduPulse' and changed in the app.py to base.html default rendering.

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "Hellow World!"
+def home():
+    return render_template('base.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
