0bd7925cb8732c212a58adffc26e01f4cacd1023
Md. Ashraf Uddin <108550504+AshrafUAsif@users.noreply.github.com>
2024-04-11 11:50:23 +0000

    The remaining paths of the html files has been created successfully.

--- app.py
+++ app.py
@@ -31,6 +31,30 @@ def course_details():
 def log_in():
     return render_template('log-in.html')
 
+@app.route('/sign-up.html')
+def sign_up():
+    return render_template('sign-up.html')
+
+@app.route('/careers.html')
+def careers():
+    return render_template('careers.html')
+
+@app.route('/blog.html')
+def blog():
+    return render_template('blog.html')
+
+@app.route('/instructor.html')
+def instructor():
+    return render_template('instructor.html')
+
+@app.route('/admin.html')
+def admin():
+    return render_template('admin.html')
+
+@app.route('/profile.html')
+def profile():
+    return render_template('profile.html')
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
