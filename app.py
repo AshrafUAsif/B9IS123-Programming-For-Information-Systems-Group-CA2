70abe9d20c5ee15f1e3f00f59324339a8bf9cc9a
Md. Ashraf Uddin <108550504+AshrafUAsif@users.noreply.github.com>
2024-04-11 11:45:37 +0000

    routes has been created using flask of the created html pages for connecting them in routes.

--- app.py
+++ app.py
@@ -6,6 +6,30 @@ app = Flask(__name__)
 def home():
     return render_template('base.html')
 
+#Adding routes for the all other pages
+@app.route('/about.html')
+def about_html():
+    return render_template('about.html')
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/pricing.html')
+def pricing():
+    return render_template('pricing.html')
+
+@app.route('/courses.html')
+def courses():
+    return render_template('courses.html')
+
+@app.route('/course-details.html')
+def course_details():
+    return render_template('course-details.html')
+
+@app.route('/log-in.html')
+def log_in():
+    return render_template('log-in.html')
 
 
 if __name__ == "__main__":
